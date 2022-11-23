f257fb80b406476f127a458956a54a1e065d4403
ahas0037 <ahas0037@student.monash.edu>
2022-11-24 00:46:08 +0800

    Implement gmail

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-app.config["config_key"] = config_value
-app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
-
-from my_first_project import main
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from my_first_project import app
+from flask import Flask
 from flask import render_template, url_for, request, redirect, flash
 from email_validator import validate_email, EmailNotValidError
+from flask_mail import Mail, Message
+import logging
+import os
 
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
+
+app.logger.setLevel(logging.DEBUG)
+
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -37,27 +54,48 @@ def form_complete():
         is_valid = True
 
         if not username:
-            flash("Enter username")
+            flash("Enter username!")
             is_valid = False
 
         if not email:
-            flash("Enter email address")
+            flash("Enter email address!")
             is_valid = False
 
         try:
             validate_email(email)
         except EmailNotValidError:
-            flash("This email format is not valid")
+            flash("This email format is not valid!")
             is_valid = False
 
         if not description:
-            flash("Enter details of enquiry")
+            flash("Enter details of enquiry!")
             is_valid = False
 
         if not is_valid:
             return redirect(url_for("form"))
 
-        flash("Your enquiry sent over via email.Thank you for contacting us!")
+        send_mail(
+            email,
+            "Thanks for contacting us",
+            "contact_mail",
+            username=username,
+            description=description
+        )
+
+        flash("Your enquiry sent over via the email! Thanks you for your opinion!")
         return redirect(url_for("form_complete"))
 
     return render_template("form_complete.html")
+
+app.logger.critical("fatal error")
+app.logger.error("error")
+app.logger.warning("warning")
+app.logger.info("info")
+app.logger.debug("debug")
+
+def send_mail(to, subject, template, **kwargs):
+    message = Message(subject, recipients=[to])
+    message.body = render_template(template + ".txt", **kwargs)
+    message.html = render_template(template + ".html", **kwargs)
+
+    mail.send(message)
